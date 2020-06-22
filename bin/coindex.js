74e9caa07d8420b305b58451fd1f393f8cb4c867
Dominat0rr <claermandominik@gmail.com>
2020-06-22 17:55:40 +0200

    Added API implementation, error handling & finish project

--- bin/coindex.js
+++ bin/coindex.js
@@ -5,6 +5,7 @@ const pkg = require("../package.json");
 program
     .version(pkg.version)
     .command("key", "Manage API Key -- https://nomics.com")
+    .command("check", "Check Coin Currency Info")
     .parse(process.argv);
 
 //console.log(process.argv)
